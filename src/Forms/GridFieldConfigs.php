9c883a9f2c1e1628dd5baff637e9a33207c689be
micschk <mic@restruct.nl>
2022-07-01 16:34:52 +0200

    Added some GF configs

--- src/Forms/GridFieldConfigs.php
+++ src/Forms/GridFieldConfigs.php
@@ -2,10 +2,18 @@
 
 namespace Restruct\Silverstripe\AdminTweaks\Forms;
 
+use App\Forms\GridFieldVersionedOrderableRows;
+use SilverStripe\Forms\GridField\GridField_ActionMenu;
+use SilverStripe\Forms\GridField\GridFieldAddNewButton;
 use SilverStripe\Forms\GridField\GridFieldButtonRow;
     use SilverStripe\Forms\GridField\GridFieldConfig;
-    use SilverStripe\Forms\GridField\GridFieldDeleteAction;
-    use SilverStripe\Forms\GridField\GridFieldToolbarHeader;
+use SilverStripe\Forms\GridField\GridFieldDataColumns;
+use SilverStripe\Forms\GridField\GridFieldDeleteAction;
+use SilverStripe\Forms\GridField\GridFieldDetailForm;
+use SilverStripe\Forms\GridField\GridFieldEditButton;
+use SilverStripe\Forms\GridField\GridFieldFilterHeader;
+use SilverStripe\Forms\GridField\GridFieldSortableHeader;
+use SilverStripe\Forms\GridField\GridFieldToolbarHeader;
     use Symbiote\GridFieldExtensions\GridFieldAddNewInlineButton;
     use Symbiote\GridFieldExtensions\GridFieldEditableColumns;
     use Symbiote\GridFieldExtensions\GridFieldOrderableRows;
@@ -16,7 +24,7 @@ class GridFieldConfigs
     /**
      * EditableOrderable gridfieldconfig
      */
-   public static function editable_orderable()
+    public static function editable_orderable()
     {
         return GridFieldConfig::create()
             ->addComponent(new GridFieldButtonRow('after'))
@@ -28,5 +36,30 @@ class GridFieldConfigs
             ->addComponent(new GridFieldAddNewInlineButton('buttons-after-left'));
     }
 
+    /**
+     * Copied from bigfork/silverstripe-recipe
+     * @param $showAdd
+     * @return GridFieldConfig
+     */
+    public static function filterable_orderable_recordeditor($showAdd = null)
+    {
+        $conf = GridFieldConfig::create()
+            ->addComponent(new GridFieldButtonRow('before'))
+            ->addComponent(new GridFieldAddNewButton('buttons-before-left'))
+            ->addComponent(new GridFieldToolbarHeader())
+            ->addComponent($sort = new GridFieldSortableHeader())
+            ->addComponent($filter = new GridFieldFilterHeader())
+            ->addComponent(new GridFieldDataColumns())
+            ->addComponent(new GridFieldEditButton())
+            ->addComponent(new GridFieldDeleteAction())
+            ->addComponent(new GridField_ActionMenu())
+            ->addComponent(new GridFieldDetailForm(null, null, $showAdd))
+            ->addComponent(new GridFieldVersionedOrderableRows());
+
+        $sort->setThrowExceptionOnBadDataType(false);
+        $filter->setThrowExceptionOnBadDataType(false);
+
+        return $conf;
+    }
 
 }
