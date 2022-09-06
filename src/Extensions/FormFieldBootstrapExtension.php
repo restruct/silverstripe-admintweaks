e707534aba722d0b12375b23304cdf58dae0affa
Michael van Schaik <mic@restruct.nl>
2022-09-06 13:59:51 +0200

    Update FormFieldBootstrapExtension.php

--- src/Extensions/FormFieldBootstrapExtension.php
+++ src/Extensions/FormFieldBootstrapExtension.php
@@ -15,18 +15,33 @@ class FormFieldBootstrapExtension extends \SilverStripe\Core\Extension
 {
     public function onBeforeRender( $field )
     {
-        $_a   = array();
-        $_a[] = TextField::class;
-        $_a[] = CurrencyField::class;
-        $_a[] = DateField::class;
-        $_a[] = EmailField::class;
-        $_a[] = PasswordField::class;
-        $_a[] = TextareaField::class;
-        $_a[] = DropdownField::class;
-        $_a[] = NumericField::class;
-
-        if ( in_array( get_class( $field ), $_a ) ) {
+        $form_control = [
+            TextField::class,
+            CurrencyField::class,
+            DateField::class,
+            EmailField::class,
+            PasswordField::class,
+            TextareaField::class,
+            NumericField::class,
+        ];
+        if ( in_array( get_class( $field ), $form_control ) ) {
             $field->addExtraClass( 'form-control' );
+        }
+
+        $form_select = [
+            DropdownField::class,
+        ];
+        if ( in_array( get_class( $field ), $form_select ) ) {
+            $field->addExtraClass( 'form-select' );
+        }
+
+        $form_check = [
+            CheckboxField::class,
+            CheckboxSetField::class,
+            OptionsetField::class,
+        ];
+        if ( in_array( get_class( $field ), $form_check ) ) {
+            $field->addExtraClass( 'form-check' );
         }
     }
 }
