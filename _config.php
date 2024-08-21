7d6cf3f9559cc394db9a0bbcc1f569e24336f2d3
Bart van Irsel <bart@webium.nl>
2024-08-21 14:43:34 +0200

    use statement gone?

--- _config.php
+++ _config.php
@@ -1,10 +1,12 @@
 <?php
 
+use SilverStripe\Core\Environment;
+
 //
 // Set system email sender via ENV (moved to _config.php because BACKTICKS ENV VARS in Yaml are only supported via Injector)
 //
 $sys_email = Environment::getEnv('APP_SYSTEM_EMAIL_ADDRESS');
 $sys_name = Environment::getEnv('APP_SYSTEM_EMAIL_SENDER');
-if($sys_email && $sys_name) {
+if ($sys_email && $sys_name) {
     SilverStripe\Control\Email\Email::config()->set('admin_email', [$sys_email => $sys_name]);
 }
