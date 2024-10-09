b5caba2b5b0f0dc0bc24349e22a92e5c9c8786b0
micschk <mic@restruct.nl>
2024-10-09 14:02:35 +0200

    Moving to CacheHelpers::load_cache instead of get_cache

--- _config.php
+++ _config.php
@@ -5,8 +5,6 @@ use SilverStripe\Core\Environment;
 //
 // Set system email sender via ENV (moved to _config.php because BACKTICKS ENV VARS in Yaml are only supported via Injector)
 //
-use SilverStripe\Core\Environment;
-
 $sys_email = Environment::getEnv('APP_SYSTEM_EMAIL_ADDRESS');
 $sys_name = Environment::getEnv('APP_SYSTEM_EMAIL_SENDER');
 if ($sys_email && $sys_name) {
