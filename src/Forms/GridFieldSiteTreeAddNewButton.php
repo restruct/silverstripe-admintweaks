459f5ab630dc073b51565f09fd0ec4037735f647
Bart van Irsel <bart@webium.nl>
2025-10-10 10:47:40 +0200

    ss6 fix

--- src/Forms/GridFieldSiteTreeAddNewButton.php
+++ src/Forms/GridFieldSiteTreeAddNewButton.php
@@ -3,9 +3,12 @@
 namespace Restruct\Silverstripe\AdminTweaks\Forms;
 
 use SilverStripe\CMS\Model\SiteTree;
+use SilverStripe\Core\ClassInfo;
 use SilverStripe\Core\Config\Config;
 use SilverStripe\Core\Injector\Injector;
 use SilverStripe\Lumberjack\Forms\GridFieldSiteTreeAddNewButton as LumberjackGridFieldSiteTreeAddNewButton;
+use SilverStripe\ORM\HiddenClass;
+
 if (!class_exists(LumberjackGridFieldSiteTreeAddNewButton::class)) {
     return;
 }
@@ -25,7 +28,8 @@ class GridFieldSiteTreeAddNewButton
             return $children;
         }
 
-        $nonHiddenPageTypes = SiteTree::page_type_classes();
+        // Get non-hidden page types (SS6 compatible - page_type_classes() was removed)
+        $nonHiddenPageTypes = $this->getNonHiddenPageTypes();
         $extraHiddenClasses = Config::inst()->get($parent->className, 'hide_from_cms_tree');
         foreach ($extraHiddenClasses as $class){
             $instance = Injector::inst()->get($class);
@@ -35,6 +39,27 @@ class GridFieldSiteTreeAddNewButton
         }
 
         return $children;
+    }
+
+    /**
+     * Get all non-hidden SiteTree subclasses
+     * Replacement for removed SiteTree::page_type_classes() in SS6
+     *
+     * @return array
+     */
+    protected function getNonHiddenPageTypes()
+    {
+        $classes = ClassInfo::subclassesFor(SiteTree::class);
+        $result = [];
+
+        foreach ($classes as $class) {
+            // Skip classes marked with HiddenClass interface
+            if (!is_a($class, HiddenClass::class, true)) {
+                $result[] = $class;
+            }
+        }
+
+        return $result;
     }
 
 
