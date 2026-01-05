3593b841539f968f1dea9ed91f328d838248e1e3
Michael van Schaik <mic@restruct.nl>
2026-01-05 13:59:58 +0100

    Use IntlDateFormatter to format seconds as time
    Replace date function with IntlDateFormatter for better localization.

--- src/Extensions/DBTimeExtension.php
+++ src/Extensions/DBTimeExtension.php
@@ -47,7 +47,11 @@ class DBTimeExtension extends Extension
      */
     public static function format_seconds_as_time($seconds, $formatString = 'H:mm')
     {
-        return date($formatString, strtotime("1-1-1970 0:00:00") + $seconds);
+        /** @var DBTime $this->owner */
+//        return date($formatString, strtotime("1-1-1970 0:00:00") + $seconds);
+        $formatter = IntlDateFormatter::create(i18n::get_locale(), IntlDateFormatter::NONE, IntlDateFormatter::MEDIUM);
+        $formatter->setPattern($formatString);
+        return $formatter->format(strtotime("1-1-1970 0:00:00") + $seconds);
     }
 
     //
