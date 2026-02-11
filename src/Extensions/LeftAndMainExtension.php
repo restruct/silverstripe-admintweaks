3a4df7721db0ab1aa5881e5bdef9210f62382ee2
micschk <mic@restruct.nl>
2026-02-11 16:05:27 +0100

    Add configurable bs-icons.css loading via YAML config
    Bootstrap Icons for .bs-icon-* button classes are now loaded via
    a LeftAndMain extension that checks the include_bs_icons config.
    Set to false in project config to disable if not using these classes.

---
+++ src/Extensions/LeftAndMainExtension.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Restruct\Silverstripe\AdminTweaks\Extensions;
+
+use SilverStripe\Core\Extension;
+use SilverStripe\View\Requirements;
+
+/**
+ * Extension for LeftAndMain to conditionally load optional CSS.
+ */
+class LeftAndMainExtension extends Extension
+{
+    /**
+     * @config
+     * Whether to include Bootstrap Icons CSS for .bs-icon-* button classes.
+     * The icon font is loaded from CDN for better browser caching.
+     */
+    private static bool $include_bs_icons = true;
+
+    public function init(): void
+    {
+        if ($this->getOwner()->config()->get('include_bs_icons')) {
+            Requirements::css('restruct/silverstripe-admintweaks:client/dist/css/bs-icons.css');
+        }
+    }
+}
